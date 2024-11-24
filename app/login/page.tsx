eb8c123a04078b3c113ebdfc4b62b9fe4035eedf
daniel petcov <daniel.petcov@dotteam.md>
2024-11-24 19:43:04 +0200

    solved problem with dep

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -62,7 +62,7 @@ export default function Login() {
       
       <Snowfall />
       
-      <div className="max-w-md w-full space-y-8 p-4 sm:p-8 bg-white/90 backdrop-blur-sm rounded-lg shadow-2xl border border-white/20 ">
+      <div className="max-w-md w-full space-y-8 p-8 bg-white/90 backdrop-blur-sm rounded-lg shadow-2xl border border-white/20 ">
         <div className="text-center space-y-2">
           <div className="flex justify-center">
             <Logo02 size={100} />
